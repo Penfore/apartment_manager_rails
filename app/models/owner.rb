1973c7a9dc62d91ebb8f3409d5db786d5ee37769
Fulvio Leo <fulviokaldas@outlook.com>
2022-11-07 10:04:33 -0300

    owner associations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,7 @@
 class Owner < ApplicationRecord
+  has_many :residents, dependent: :destroy
+  has_many :vehicles, dependent: :destroy
+  has_many :pets, dependent: :destroy
+  has_many :employees, dependent: :destroy
+  has_many :emergencies, dependent: :destroy
 end
